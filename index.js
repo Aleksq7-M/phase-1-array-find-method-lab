81f26fb48dddcb44d75eefd1698b0c455f98eb88
aleksq7 <morrisalex2001@gmail.com>
2021-08-11 14:28:39 -0600

    commit tree 59d06416b7afe2e58e75ef6c2fea0ab3c7636432

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+function superbowlWin(array){
+    const winningYear = array.find(element => element.result === 'W')
+    console.log('LOG:', winningYear);
+    if (winningYear !== undefined){
+        return winningYear.year;
+    }
+}
